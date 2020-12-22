4d4ae8f887cc26ee783fb075d7651a29b1aa7a03
Maciej <maciejmad16@gmail.com>
2020-12-22 19:37:42 +0000

    Function of adding numbered divs is finished - very short

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,14 @@
 const press = document.querySelector("button");
-press.addEventListener("click", divAdding());
-
+press.addEventListener("click", divAdding)
+let number = 1;
 function divAdding() {
     console.log('click');
     const container = document.querySelector(".container");
     let div1 = createTag("div", "class", "squareDiv");
-    div1.innerText = `1`;
+    div1.textContent = number;
+    number++;
+    console.log(number);
+
     container.appendChild(div1);
 }
 
