47f22756962bf2bc0e339c575c459cc9cefd09b5
Maciej <maciejmad16@gmail.com>
2020-12-23 18:02:17 +0000

    Created li adding function structure

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,6 @@ press.addEventListener("click", divAdding)
 let number = 1;
 
 function divAdding() {
-    console.log('click');
     const container = document.querySelector(".container");
     let div1 = createTag("div", "class", "squareDiv");
     div1.textContent = number;
@@ -12,6 +11,24 @@ function divAdding() {
     }
     number++;
     container.appendChild(div1);
+}
+
+const addRecord = document.querySelector(".list");
+addRecord.addEventListener("click", recordAdding)
+let number2 = 1;
+
+function recordAdding() {
+    console.log('click');
+    const container2 = document.querySelector(".container2");
+    let unorderedList = createTag("ul", "class", "");
+    let listElement = createTag("li", "class", "");
+    listElement.textContent = number;
+    if (number % 2 == 0) {
+        listElement.setAttribute("class", "secondLi");
+    }
+    number2++;
+    unorderedList.appendChild(listElement);
+    container2.appendChild(unorderedList);
 }
 
 function createTag(tag, attribute, value) {
