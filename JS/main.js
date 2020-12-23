2d20ce50c80967b91eb9bb74a2c917330fd25019
Maciej <maciejmad16@gmail.com>
2020-12-23 16:40:12 +0000

    Added  condition to chane class for each fith element

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,16 @@
 const press = document.querySelector("button");
 press.addEventListener("click", divAdding)
 let number = 1;
+
 function divAdding() {
     console.log('click');
     const container = document.querySelector(".container");
     let div1 = createTag("div", "class", "squareDiv");
     div1.textContent = number;
+    if (div1.textContent % 5 == 0) {
+        div1.setAttribute("class", "roundedDiv");
+    }
     number++;
-    console.log(number);
-
     container.appendChild(div1);
 }
 
