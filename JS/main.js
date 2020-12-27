b060705248c0a890b833c302d594bc76c580845d
Maciej <maciejmad16@gmail.com>
2020-12-27 04:49:39 +0000

    Added function of scrolling squere

--- JS/main.js
+++ JS/main.js
@@ -43,9 +43,27 @@ function createTag(tag, attribute, value) {
 
 const squere = createTag("div", "class", "scroll");
 document.body.appendChild(squere);
-let size = "10";
+let size = 10;
 squere.style.width = size + "px";
 squere.style.height = size + "px";
 
 
-window.addEventListener("scroll")
+window.addEventListener("scroll", sizeChange);
+let flag = true;
+function sizeChange() {
+    if (flag == true) {
+        size += 10;
+        squere.style.width = size + "px";
+        squere.style.height = size + "px";
+    } else {
+        size -= 10;
+        squere.style.width = size + "px";
+        squere.style.height = size + "px";
+    }
+
+    if (size >= window.innerWidth / 2) {
+        flag = false;
+    } else if (size <= 0) {
+        flag = true;
+    }
+}
